fb9e36ec66b74c5ea7528f71cd98015af57f5457
yeargin2021 <yeargin2021@gmail.com>
2021-10-02 05:20:21 -0400

    First commit

---
+++ PyClass01/Mod01.py
@@ -0,0 +1,28 @@
+"""
+Python OOP class example: Dog
+By: Tommy H. Yeargin, Jr.
+Created: 02 Oct 2021
+"""
+
+class Dog:
+
+    def __init__(self, DogName):
+        self.DogName = DogName
+    
+    def bark(self):
+        """Enables the dog you create to bark."""
+        print(self.DogName + " says, \"Woof!\"")
+
+    def about():
+        """
+        Python OOP example: Dog
+        By: Tommy H. Yeargin, Jr.
+        Created: 02 Oct 2021
+        """
+
+if __name__ == "__main__":
+
+    fido = Dog("Fido")
+
+    # print(fido.DogName)
+    fido.bark()
---
+++ PyClass01/importMod01.py
@@ -0,0 +1,7 @@
+from Mod01 import Dog
+
+peewee = Dog("Pee Wee")
+
+# peewee.bark()
+# print(help(Dog))
+print(peewee.DogName)
