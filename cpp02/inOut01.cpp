b77aef08bb77ac7f0d2142f218f22271331ee87c
yeargin2021 <yeargin2021@gmail.com>
2021-10-07 09:45:44 -0400

    Learning c++ again...

---
+++ cpp02/inOut01.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    double pi = 3.1415;
+    double diameter = 12.0;
+
+    std::cout << "The circumference of a circle " << diameter << " inches in diameter is " << pi * diameter << " inches.\n";
+}
